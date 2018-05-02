732afa9a4e409d13f7cd9daa332e1bfef9065fb3
Unknown <irvan.2208@gmail.com>
2018-05-03 06:43:58 +0700

    Update: koneksi

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "irvansan_flowchart_generator";
+$password = "flowchart_generator";
+$dbname = "irvansan_flowchart_generator";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// } 
+// echo "Connected successfully";
+?>
